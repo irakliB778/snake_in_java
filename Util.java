d32572d55d71976c88afa1fc6013732ec6196850
Irakli <iraklibarbaqadze241995@gmail.com>
2021-01-20 12:10:25 +0400

    change main code structure, add bounsing function, add also restart game

--- Util.java
+++ Util.java
@@ -1,12 +1,18 @@
 import java.util.Random;
 import java.awt.*;
-public class Util {
-    
+public class Util extends GameFeature {
    public Color createColor(){
         int red=new Random().nextInt((int)255);
         int green=new Random().nextInt((int)255);
         int blue=new Random().nextInt((int)255);
 
         return new Color(red,green,blue);
+    }
+    public void drawLine(){
+        for(int i=0; i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            graphic.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            graphic.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+            graphic.setColor(Color.CYAN);
+        }
     }
 }
--- tail.java
+++ tail.java
@@ -1,36 +1,48 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.awt.*;
 
-public class tail implements gameFeature {
+public class Tail  {
     ArrayList<TailBlock> tailBody = new ArrayList<TailBlock>();
-    int initiallength=6;
+    int initiallength=30;
+    
 
     public int getTailLength(){
         return tailBody.size()-1;
     }
+    
     public TailBlock getBlock(int index){
         return tailBody.get(index);
     }
+
     public void addBlock(){
         tailBody.add(new TailBlock(0, 0));
     }
-    public void createInitialTail(){
-        
-        for(int i=0; i<=initiallength;i++){
-           int x=(initiallength-1-i)*UNIT_SIZE;
-           int y=0;
-           TailBlock tailB=new TailBlock(x,y);
-           //System.out.println(x);
-           tailBody.add(tailB);
-        }
-        
-    }
+
     public void blinkColor(){
         for(int i=1;i<=getTailLength();i++){
             tailBody.get(i).createColor();
         }
     }
+    public void paint(){
+        
+        for(int i=0;i<this.getTailLength();i++){
+            TailBlock block = this.getBlock(i);
+            block.paint(i);
+        }
+    }
+
+    public void createInitialTail(){
+        
+        for(int i=0; i<=initiallength; i++){
+           int x=(initiallength-1-i)*GameFeature.UNIT_SIZE;
+           int y=0;
+           TailBlock tailBlock=new TailBlock(x,y);
+           //System.out.println(x);
+           tailBody.add(tailBlock);
+        }
+        
+    }
+    
     public void updateTail(){
         for(int i=getTailLength(); i>0;i--){
             TailBlock currentBlock = tailBody.get(i);
@@ -43,9 +55,11 @@ public class tail implements gameFeature {
     }
 
 }
-class TailBlock{
+class TailBlock extends GameFeature{
     int x;
     int y;
+    int width=UNIT_SIZE;
+    int height=UNIT_SIZE;
     Color color;
     Util util;
     TailBlock(int x, int y){
@@ -54,6 +68,15 @@ class TailBlock{
         util = new Util();
         color = util.createColor();
 
+    }
+    public void paint(int index){
+        if(index==0){
+            graphic.setColor(Color.green);
+            graphic.fillRect(this.x, this.y, UNIT_SIZE, UNIT_SIZE);
+        }else{
+            graphic.setColor(this.color);
+            graphic.fillRect(this.x, this.y, UNIT_SIZE, UNIT_SIZE);
+        }
     }
     public void createColor(){
         color = util.createColor();
